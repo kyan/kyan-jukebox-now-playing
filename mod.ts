e420ac218decd7bac9ac67ad1f8afe290b2ed562
Duncan Robertson <duncan@kyan.com>
2021-11-10 13:31:49 +0000

    Add simple json version

--- mod.ts
+++ mod.ts
@@ -1,28 +1,13 @@
-import {
-  json,
-  serve,
-  validateRequest,
-} from "https://deno.land/x/sift@0.1.7/mod.ts";
+import { listenAndServe } from "https://deno.land/std/http/server.ts";
 import { MongoClient } from "https://deno.land/x/mongo@v0.26.0/mod.ts";
 import { SettingsSchema } from "./types.ts";
 
 async function handleRequest(request: Request) {
-  const { error } = await validateRequest(request, {
-    POST: {},
-  });
-
-  if (error) {
-    return json(
-      { response_type: "ephemeral", text: error.message },
-      { status: error.status },
-    );
-  }
-
   const mongoUrl = Deno.env.get("MONGODB_URL");
   if (!mongoUrl) {
-    return json({
-      response_type: "ephemeral",
-      text: "Environment variable `MONGODB_URL` not set.",
+    return new Response(null, {
+      status: 422,
+      statusText: "Environment variable `MONGODB_URL` not set.",
     });
   }
   const client = new MongoClient();
@@ -33,22 +18,44 @@ async function handleRequest(request: Request) {
     const db = client.database("jukebox-production");
     const settings = db.collection<SettingsSchema>("settings");
     const slackMessage = await settings.findOne({ key: "slack" }, { noCursorTimeout: false });
+    const currentTrack = slackMessage?.value?.currentTrack;
 
-    return json(slackMessage?.value?.currentTrack);
+    if (request.method == "POST") {
+      return new Response(JSON.stringify(currentTrack), {
+        headers: {
+          "content-type": "application/json; charset=UTF-8",
+        },
+      });
+    }
+
+    const [title, artist, album] = currentTrack.blocks[2].text.text.split("\n")
+    const image = currentTrack.blocks[2].accessory.image_url
+    const nowPlaying = {
+      title: title.trim().replace(/\*/g, ""),
+      artist: artist.trim(),
+      album: album.trim(),
+      image,
+      rating: currentTrack.blocks[4].fields[0].text.replace(/\*/g, ""),
+      "voted_by": currentTrack.blocks[4].fields[1].text.replace(/\*/g, ""),
+      played: currentTrack.blocks[4].fields[2].text.replace(/\*/g, ""),
+      "last_played": currentTrack.blocks[4].fields[3].text.replace(/\*/g, ""),
+      "added_by": currentTrack.blocks[5].elements[0].text
+    };
+    return new Response(JSON.stringify(nowPlaying), {
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    });
   } catch (error) {
     // If something goes wrong in the above block, let's log the error
     // and return a generic error to the user.
     console.log(error);
-    return json(
-      {
-        response_type: "ephemeral",
-        text: "Error fetching the jukebox data. Please try again.",
-      },
-      { status: 500 },
-    );
+    return new Response(null, {
+      status: 500,
+      statusText: "Error fetching the jukebox data. Please try again.",
+    });
   }
 }
 
-serve({
-  "/": handleRequest,
-});
+console.log("Listening on http://localhost:8080");
+await listenAndServe(":8080", handleRequest);
